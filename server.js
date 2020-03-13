81b53dde095ba421a01cd75c25433b61d5e84f77
jdab2020 <jdabalos90@gmail.com>
2020-03-12 20:45:50 -0700

    updated server.js | added html-routes

--- server.js
+++ server.js
@@ -18,7 +18,10 @@ app.use(express.static("public"));
 require("./routes/api-routes.js")(app);
 require("./routes/html-routes.js")(app);
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/populatedb", { useNewUrlParser: true });
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", { 
+  useNewUrlParser: true,
+  useFindAndModify: false
+});
 
 
 
