bef65a485429c96e50f79d54d42eea96b40f7580
jdab2020 <jdabalos90@gmail.com>
2020-03-12 23:03:13 -0700

    updated api routes

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -12,12 +12,21 @@ module.exports = function (app) {
     })
 
     app.put("/api/workouts/:id", (req, res) => {
-
+        db.Workout.findOneAndUpdate(
+            { _id: req.params.id },
+            { $push: { exercises: req.body } }
+        )
+            .then(results => {
+                res.json(results);
+            })
+            .catch(err => {
+                res.json(err);
+            })
     })
 
-    app.post("/api/workouts", ({body}, res) => {
-        db.Workout.create({body})
-            .then( results => {
+    app.post("/api/workouts", ({ body }, res) => {
+        db.Workout.create({ body })
+            .then(results => {
                 res.json(results);
             })
             .catch(err => {
