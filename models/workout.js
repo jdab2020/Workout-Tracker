5bb62d0ac1c86d4a8467ec116fca534f24ea53b1
jdab2020 <jdabalos90@gmail.com>
2020-03-12 19:39:49 -0700

    added models/workout

---
+++ models/workout.js
@@ -0,0 +1,49 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const WorkoutSchema = new Schema (
+    {
+        day: {
+            type: Date,
+            default: Date.now
+        },
+
+        excercises: [
+            {
+                type: {
+                    type: String,
+                    trim: true,
+                    required: "Enter exercise type"
+                },
+
+                name: {
+                    type: String,
+                    trim: true,
+                    required: "Enter exercise name"
+                },
+
+                duration: {
+                    type: Number,
+                    required: "Enter exercise duration (minutes)"
+                },
+
+                weight: {
+                    type: Number,
+                },
+
+                reps: {
+                    type: Number,
+                },
+
+                sets: {
+                    type: Number,
+                },
+
+                distance: {
+                    type: Number
+                }
+            }
+        ]
+    }    
+)
